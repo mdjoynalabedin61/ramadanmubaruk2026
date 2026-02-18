61e16ef2f6f4af14fe3ff3aad8b2b9722c443717
Mohammad_Joynal_Abedin <123541614+mdjoynalabedin61@users.noreply.github.com>
2026-02-19 01:00:07 +0600

    Refactor ramadanData structure and update functions

--- script.js
+++ script.js
@@ -1,63 +1,303 @@
-
-const ramadanData = [
-  { "id": 1, "ramzanNumber": "০১", "date": "১৯ ফেব্রুয়ারি", "dayname": "বৃহস্পতিবার", "sehri": "05:13:00", "fajr": "05:19:00", "iftar": "18:04:00" },
-  { "id": 2, "ramzanNumber": "০২", "date": "২০ ফেব্রুয়ারি", "dayname": "শুক্রবার", "sehri": "05:12:00", "fajr": "05:18:00", "iftar": "18:05:00" },
-  { "id": 3, "ramzanNumber": "০৩", "date": "২১ ফেব্রুয়ারি", "dayname": "শনিবার", "sehri": "05:11:00", "fajr": "05:17:00", "iftar": "18:06:00" },
-  { "id": 4, "ramzanNumber": "০৪", "date": "২২ ফেব্রুয়ারি", "dayname": "রবিবার", "sehri": "05:10:00", "fajr": "05:16:00", "iftar": "18:06:00" },
-  { "id": 5, "ramzanNumber": "০৫", "date": "২৩ ফেব্রুয়ারি", "dayname": "সোমবার", "sehri": "05:10:00", "fajr": "05:15:00", "iftar": "18:07:00" },
-  { "id": 6, "ramzanNumber": "০৬", "date": "২৪ ফেব্রুয়ারি", "dayname": "মঙ্গলবার", "sehri": "05:09:00", "fajr": "05:15:00", "iftar": "18:07:00" },
-  { "id": 7, "ramzanNumber": "০৭", "date": "২৫ ফেব্রুয়ারি", "dayname": "বুধবার", "sehri": "05:08:00", "fajr": "05:14:00", "iftar": "18:08:00" },
-  { "id": 8, "ramzanNumber": "০৮", "date": "২৬ ফেব্রুয়ারি", "dayname": "বৃহস্পতিবার", "sehri": "05:07:00", "fajr": "05:13:00", "iftar": "18:08:00" },
-  { "id": 9, "ramzanNumber": "০৯", "date": "২৭ ফেব্রুয়ারি", "dayname": "শুক্রবার", "sehri": "05:06:00", "fajr": "05:12:00", "iftar": "18:09:00" },
-  { "id": 10, "ramzanNumber": "১০", "date": "২৮ ফেব্রুয়ারি", "dayname": "শনিবার", "sehri": "05:06:00", "fajr": "05:11:00", "iftar": "18:09:00" },
-  { "id": 11, "ramzanNumber": "১১", "date": "০১ মার্চ", "dayname": "রবিবার", "sehri": "05:05:00", "fajr": "05:11:00", "iftar": "18:10:00" },
-  { "id": 12, "ramzanNumber": "১২", "date": "০২ মার্চ", "dayname": "সোমবার", "sehri": "05:04:00", "fajr": "05:10:00", "iftar": "18:10:00" },
-  { "id": 13, "ramzanNumber": "১৩", "date": "০৩ মার্চ", "dayname": "মঙ্গলবার", "sehri": "05:03:00", "fajr": "05:09:00", "iftar": "18:11:00" },
-  { "id": 14, "ramzanNumber": "১৪", "date": "০৪ মার্চ", "dayname": "বুধবার", "sehri": "05:02:00", "fajr": "05:08:00", "iftar": "18:11:00" },
-  { "id": 15, "ramzanNumber": "১৫", "date": "০৫ মার্চ", "dayname": "বৃহস্পতিবার", "sehri": "05:01:00", "fajr": "05:08:00", "iftar": "18:11:00" },
-  { "id": 16, "ramzanNumber": "১৬", "date": "০৬ মার্চ", "dayname": "শুক্রবার", "sehri": "05:00:00", "fajr": "05:07:00", "iftar": "18:12:00" },
-  { "id": 17, "ramzanNumber": "১৭", "date": "০৭ মার্চ", "dayname": "শনিবার", "sehri": "05:00:00", "fajr": "05:06:00", "iftar": "18:12:00" },
-  { "id": 18, "ramzanNumber": "১৮", "date": "০৮ মার্চ", "dayname": "রবিবার", "sehri": "04:59:00", "fajr": "05:05:00", "iftar": "18:13:00" },
-  { "id": 19, "ramzanNumber": "১৯", "date": "০৯ মার্চ", "dayname": "সোমবার", "sehri": "04:58:00", "fajr": "05:04:00", "iftar": "18:14:00" },
-  { "id": 20, "ramzanNumber": "২০", "date": "১০ মার্চ", "dayname": "মঙ্গলবার", "sehri": "04:57:00", "fajr": "05:03:00", "iftar": "18:14:00" },
-  { "id": 21, "ramzanNumber": "২১", "date": "১১ মার্চ", "dayname": "বুধবার", "sehri": "04:55:00", "fajr": "05:01:00", "iftar": "18:14:00" },
-  { "id": 22, "ramzanNumber": "২২", "date": "১২ মার্চ", "dayname": "বৃহস্পতিবার", "sehri": "04:54:00", "fajr": "05:01:00", "iftar": "18:15:00" },
-  { "id": 23, "ramzanNumber": "২৩", "date": "১৩ মার্চ", "dayname": "শুক্রবার", "sehri": "04:53:00", "fajr": "05:00:00", "iftar": "18:15:00" },
-  { "id": 24, "ramzanNumber": "২৪", "date": "১৪ মার্চ", "dayname": "শনিবার", "sehri": "04:54:00", "fajr": "04:54:00", "iftar": "18:16:00" },
-  { "id": 25, "ramzanNumber": "২৫", "date": "১৫ মার্চ", "dayname": "রবিবার", "sehri": "04:52:00", "fajr": "04:58:00", "iftar": "18:16:00" },
-  { "id": 26, "ramzanNumber": "২৬", "date": "১৬ মার্চ", "dayname": "সোমবার", "sehri": "04:51:00", "fajr": "04:57:00", "iftar": "18:17:00" },
-  { "id": 27, "ramzanNumber": "২৭", "date": "১৭ মার্চ", "dayname": "মঙ্গলবার", "sehri": "04:50:00", "fajr": "04:56:00", "iftar": "18:17:00" },
-  { "id": 28, "ramzanNumber": "২৮", "date": "১৮ মার্চ", "dayname": "বুধবার", "sehri": "04:48:00", "fajr": "04:57:00", "iftar": "18:18:00" },
-  { "id": 29, "ramzanNumber": "২৯", "date": "১৯ মার্চ", "dayname": "বৃহস্পতিবার", "sehri": "04:55:00", "fajr": "04:55:00", "iftar": "18:18:00" },
-  { "id": 30, "ramzanNumber": "৩০", "date": "২০ মার্চ", "dayname": "শুক্রবার", "sehri": "04:43:00", "fajr": "04:53:00", "iftar": "18:19:00" }
-];
+const ramadanData = [{
+    "id": 1,
+    "ramzanNumber": "০১",
+    "date": "১৯ ফেব্রুয়ারি",
+    "dayname": "বৃহস্পতিবার",
+    "sehri": "05:13:00",
+    "fajr": "05:19:00",
+    "iftar": "18:04:00"
+},
+    {
+        "id": 2,
+        "ramzanNumber": "০২",
+        "date": "২০ ফেব্রুয়ারি",
+        "dayname": "শুক্রবার",
+        "sehri": "05:12:00",
+        "fajr": "05:18:00",
+        "iftar": "18:05:00"
+    },
+    {
+        "id": 3,
+        "ramzanNumber": "০৩",
+        "date": "২১ ফেব্রুয়ারি",
+        "dayname": "শনিবার",
+        "sehri": "05:11:00",
+        "fajr": "05:17:00",
+        "iftar": "18:06:00"
+    },
+    {
+        "id": 4,
+        "ramzanNumber": "০৪",
+        "date": "২২ ফেব্রুয়ারি",
+        "dayname": "রবিবার",
+        "sehri": "05:10:00",
+        "fajr": "05:16:00",
+        "iftar": "18:06:00"
+    },
+    {
+        "id": 5,
+        "ramzanNumber": "০৫",
+        "date": "২৩ ফেব্রুয়ারি",
+        "dayname": "সোমবার",
+        "sehri": "05:10:00",
+        "fajr": "05:16:00",
+        "iftar": "18:07:00"
+    },
+    {
+        "id": 6,
+        "ramzanNumber": "০৬",
+        "date": "২৪ ফেব্রুয়ারি",
+        "dayname": "মঙ্গলবার",
+        "sehri": "05:09:00",
+        "fajr": "05:15:00",
+        "iftar": "18:07:00"
+    },
+    {
+        "id": 7,
+        "ramzanNumber": "০৭",
+        "date": "২৫ ফেব্রুয়ারি",
+        "dayname": "বুধবার",
+        "sehri": "05:08:00",
+        "fajr": "05:14:00",
+        "iftar": "18:08:00"
+    },
+    {
+        "id": 8,
+        "ramzanNumber": "০৮",
+        "date": "২৬ ফেব্রুয়ারি",
+        "dayname": "বৃহস্পতিবার",
+        "sehri": "05:07:00",
+        "fajr": "05:13:00",
+        "iftar": "18:08:00"
+    },
+    {
+        "id": 9,
+        "ramzanNumber": "০৯",
+        "date": "২৭ ফেব্রুয়ারি",
+        "dayname": "শুক্রবার",
+        "sehri": "05:06:00",
+        "fajr": "05:12:00",
+        "iftar": "18:09:00"
+    },
+    {
+        "id": 10,
+        "ramzanNumber": "১০",
+        "date": "২৮ ফেব্রুয়ারি",
+        "dayname": "শনিবার",
+        "sehri": "05:06:00",
+        "fajr": "05:12:00",
+        "iftar": "18:09:00"
+    },
+    {
+        "id": 11,
+        "ramzanNumber": "১১",
+        "date": "০১ মার্চ",
+        "dayname": "রবিবার",
+        "sehri": "05:05:00",
+        "fajr": "05:11:00",
+        "iftar": "18:10:00"
+    },
+    {
+        "id": 12,
+        "ramzanNumber": "১২",
+        "date": "০২ মার্চ",
+        "dayname": "সোমবার",
+        "sehri": "05:04:00",
+        "fajr": "05:10:00",
+        "iftar": "18:10:00"
+    },
+    {
+        "id": 13,
+        "ramzanNumber": "১৩",
+        "date": "০৩ মার্চ",
+        "dayname": "মঙ্গলবার",
+        "sehri": "05:03:00",
+        "fajr": "05:10:00",
+        "iftar": "18:11:00"
+    },
+    {
+        "id": 14,
+        "ramzanNumber": "১৪",
+        "date": "০৪ মার্চ",
+        "dayname": "বুধবার",
+        "sehri": "05:02:00",
+        "fajr": "05:09:00",
+        "iftar": "18:11:00"
+    },
+    {
+        "id": 15,
+        "ramzanNumber": "১৫",
+        "date": "০৫ মার্চ",
+        "dayname": "বৃহস্পতিবার",
+        "sehri": "05:01:00",
+        "fajr": "05:08:00",
+        "iftar": "18:11:00"
+    },
+    {
+        "id": 16,
+        "ramzanNumber": "১৬",
+        "date": "০৬ মার্চ",
+        "dayname": "শুক্রবার",
+        "sehri": "05:00:00",
+        "fajr": "05:07:00",
+        "iftar": "18:12:00"
+    },
+    {
+        "id": 17,
+        "ramzanNumber": "১৭",
+        "date": "০৭ মার্চ",
+        "dayname": "শনিবার",
+        "sehri": "04:59:00",
+        "fajr": "05:06:00",
+        "iftar": "18:12:00"
+    },
+    {
+        "id": 18,
+        "ramzanNumber": "১৮",
+        "date": "০৮ মার্চ",
+        "dayname": "রবিবার",
+        "sehri": "04:58:00",
+        "fajr": "05:05:00",
+        "iftar": "18:13:00"
+    },
+    {
+        "id": 19,
+        "ramzanNumber": "১৯",
+        "date": "০৯ মার্চ",
+        "dayname": "সোমবার",
+        "sehri": "04:57:00",
+        "fajr": "05:04:00",
+        "iftar": "18:13:00"
+    },
+    {
+        "id": 20,
+        "ramzanNumber": "২০",
+        "date": "১০ মার্চ",
+        "dayname": "মঙ্গলবার",
+        "sehri": "04:56:00",
+        "fajr": "05:03:00",
+        "iftar": "18:14:00"
+    },
+    {
+        "id": 21,
+        "ramzanNumber": "২১",
+        "date": "১১ মার্চ",
+        "dayname": "বুধবার",
+        "sehri": "04:55:00",
+        "fajr": "05:02:00",
+        "iftar": "18:14:00"
+    },
+    {
+        "id": 22,
+        "ramzanNumber": "২২",
+        "date": "১২ মার্চ",
+        "dayname": "বৃহস্পতিবার",
+        "sehri": "04:54:00",
+        "fajr": "05:01:00",
+        "iftar": "18:15:00"
+    },
+    {
+        "id": 23,
+        "ramzanNumber": "২৩",
+        "date": "১৩ মার্চ",
+        "dayname": "শুক্রবার",
+        "sehri": "04:53:00",
+        "fajr": "05:00:00",
+        "iftar": "18:15:00"
+    },
+    {
+        "id": 24,
+        "ramzanNumber": "২৪",
+        "date": "১৪ মার্চ",
+        "dayname": "শনিবার",
+        "sehri": "04:52:00",
+        "fajr": "04:59:00",
+        "iftar": "18:16:00"
+    },
+    {
+        "id": 25,
+        "ramzanNumber": "২৫",
+        "date": "১৫ মার্চ",
+        "dayname": "রবিবার",
+        "sehri": "04:51:00",
+        "fajr": "04:58:00",
+        "iftar": "18:16:00"
+    },
+    {
+        "id": 26,
+        "ramzanNumber": "২৬",
+        "date": "১৬ মার্চ",
+        "dayname": "সোমবার",
+        "sehri": "04:50:00",
+        "fajr": "04:57:00",
+        "iftar": "18:17:00"
+    },
+    {
+        "id": 27,
+        "ramzanNumber": "২৭",
+        "date": "১৭ মার্চ",
+        "dayname": "মঙ্গলবার",
+        "sehri": "04:49:00",
+        "fajr": "04:56:00",
+        "iftar": "18:17:00"
+    },
+    {
+        "id": 28,
+        "ramzanNumber": "২৮",
+        "date": "১৮ মার্চ",
+        "dayname": "বুধবার",
+        "sehri": "04:48:00",
+        "fajr": "04:55:00",
+        "iftar": "18:17:00"
+    },
+    {
+        "id": 29,
+        "ramzanNumber": "২৯",
+        "date": "১৯ মার্চ",
+        "dayname": "বৃহস্পতিবার",
+        "sehri": "04:47:00",
+        "fajr": "04:54:00",
+        "iftar": "18:18:00"
+    },
+    {
+        "id": 30,
+        "ramzanNumber": "৩০",
+        "date": "২০ মার্চ",
+        "dayname": "শুক্রবার",
+        "sehri": "04:46:00",
+        "fajr": "04:53:00",
+        "iftar": "18:18:00"
+    }];
 
 function convertTo12HourFormat(time) {
-    if(!time) return "N/A";
-    let [hour, minute] = time.split(":").map(Number);
-    let period = hour >= 12 ? "PM" : "AM";
+    if (!time) return "N/A";
+    let [hour,
+        minute] = time.split(":").map(Number);
+    let period = hour >= 12 ? "PM": "AM";
     hour = hour % 12 || 12;
     return `${hour}:${minute.toString().padStart(2, '0')} ${period}`;
 }
 
-// আজকের তারিখ নির্ধারণ ও বানান সংশোধন
-const now = new Date();
-let todayDateStr = now.toLocaleDateString('bn-BD', { day: 'numeric', month: 'long' }).trim();
 
-// বানান ফিক্স: "ফেব্রুয়ারী" কে "ফেব্রুয়ারি" করা
+const now = new Date();
+let todayDateStr = now.toLocaleDateString('bn-BD', {
+    day: 'numeric', month: 'long'
+}).trim();
+
+
 todayDateStr = todayDateStr.replace("ফেব্রুয়ারী", "ফেব্রুয়ারি");
-// কিছু ব্রাউজারে স্পেসের সমস্যা হয়, তাই সব স্পেসকে নরমাল স্পেস করা
-todayDateStr = todayDateStr.replace(/\s+/g, ' '); 
+
+todayDateStr = todayDateStr.replace(/\s+/g, ' ');
 
 
 function updateTable() {
     const tableBody = document.getElementById("ramadanTable");
-    tableBody.innerHTML = ""; // আগের ডেটা ক্লিয়ার করা
+    tableBody.innerHTML = ""; 
 
     ramadanData.forEach((day) => {
         let row = document.createElement("tr");
+
         
-        // আপনার ডেটার সাথে আজকের তারিখ তুলনা
         if (day.date.trim() === todayDateStr) {
             row.classList.add("today");
 
@@ -71,12 +311,12 @@ function updateTable() {
         }
 
         row.innerHTML = `
-            <td>${day.ramzanNumber}</td>
-            <td>${day.date}</td>
-            <td>${day.dayname}</td>
-            <td>${convertTo12HourFormat(day.sehri)}</td>
-            <td>${day.fajr ? convertTo12HourFormat(day.fajr) : "N/A"}</td>
-            <td>${convertTo12HourFormat(day.iftar)}</td>
+        <td>${day.ramzanNumber}</td>
+        <td>${day.date}</td>
+        <td>${day.dayname}</td>
+        <td>${convertTo12HourFormat(day.sehri)}</td>
+        <td>${day.fajr ? convertTo12HourFormat(day.fajr): "N/A"}</td>
+        <td>${convertTo12HourFormat(day.iftar)}</td>
         `;
         tableBody.appendChild(row);
     });
@@ -93,8 +333,10 @@ function startCountdown(sehriTimeStr, iftarTimeStr) {
     function updateCountdown() {
         const now = new Date();
         const y = now.getFullYear();
-        const m = (now.getMonth() + 1).toString().padStart(2, '0');
-        const d = now.getDate().toString().padStart(2, '0');
+        const m = (now.getMonth() + 1).toString().padStart(2,
+            '0');
+        const d = now.getDate().toString().padStart(2,
+            '0');
         const datePrefix = `${y}-${m}-${d}T`;
 
         let sehriTime = new Date(datePrefix + sehriTimeStr);
@@ -108,7 +350,7 @@ function startCountdown(sehriTimeStr, iftarTimeStr) {
             targetTime = iftarTime;
             eventName = "ইফতারের বাকি";
         } else {
-            // ইফতারের পর আগামীকালের সেহরি (ঐচ্ছিক)
+            
             document.getElementById("countdown").innerText = "আজকের ইফতার সম্পন্ন হয়েছে";
             return;
         }
@@ -118,13 +360,13 @@ function startCountdown(sehriTimeStr, iftarTimeStr) {
         const minutes = Math.floor((diff % (1000 * 60 * 60)) / (1000 * 60));
         const seconds = Math.floor((diff % (1000 * 60)) / 1000);
 
-        document.getElementById("countdown").innerText = 
-            `${eventName}: ${getBanglaNumber(hours)} ঘণ্টা ${getBanglaNumber(minutes)} মিনিট ${getBanglaNumber(seconds)} সেকেন্ড`;
+        document.getElementById("countdown").innerText =
+        `${eventName}: ${getBanglaNumber(hours)} ঘণ্টা ${getBanglaNumber(minutes)} মিনিট ${getBanglaNumber(seconds)} সেকেন্ড`;
     }
 
     updateCountdown();
     window.countdownTimer = setInterval(updateCountdown, 1000);
 }
 
-// ফাংশন রান করুন
+
 updateTable();
